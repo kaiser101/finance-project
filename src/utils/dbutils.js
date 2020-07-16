fc8283fbf732810d14bdfcd2c46e6613c138aa77
kaiser101 <kaiser101@gmail.com>
2020-07-16 09:07:00 +0530

    Small changes

--- src/utils/dbutils.js
+++ src/utils/dbutils.js
@@ -1,9 +1,32 @@
 import { Database } from "arangojs";
+import _ from "lodash";
+import createLogger from "./logger";
 
 const getConnection = () => new Database("http://localhost:8529");
 
 const getEquities = () => getConnection().collection("equities");
 
-const save = async (data) => await getEquities().save(data);
+const logger = createLogger();
 
-export { getConnection, getEquities, save };
+const save = async (data) => {
+    let saved = await getEquities().save(data);
+    logger.debug(saved);
+
+    return saved;
+};
+
+const update = async (object, delta) => {
+    let updated = await getEquities().update(object, delta);
+    logger.debug(updated);
+
+    return updated;
+};
+
+const getById = async (id) => {
+    let list = await getEquities().list([id]);
+    logger.debug(list);
+
+    return list;
+};
+
+export { getConnection, getEquities, save, update, getById };
--- src/utils/dbutils.spec.js
+++ src/utils/dbutils.spec.js
@@ -1,6 +1,17 @@
-import { getConnection, getEquities, save } from "./dbutils";
+import { getConnection, getEquities, save, update, getById } from "./dbutils";
+// configure("./filename");
 
 describe("This file contains all db utility methods", () => {
+    beforeEach(() => {
+        let equity = {
+            isin: "BSE",
+            quantity: 2000,
+            unitPrice: 250.25,
+        };
+
+        getEquities().save(equity);
+    });
+
     it("Returns a database connection", () => {
         let connection = getConnection();
         expect(connection).toBeTruthy();
@@ -21,5 +32,33 @@ describe("This file contains all db utility methods", () => {
         save(equity).then((response) =>
             expect(response._id).toBeGreaterThan(0)
         );
+    });
+
+    it("Updates the equities object", () => {
+        let equity = {
+            isin: "BSE",
+            quantity: 2000,
+            unitPrice: 250.25,
+        };
+
+        let delta = {
+            quantity: 2500,
+        };
+
+        save(equity).then((response) => {
+            expect(response._id).toBeGreaterThan(0);
+            update(equity, delta).then((response) =>
+                expect(response.quantity).toEqual(2500)
+            );
+        });
+    });
+
+    it("Gets the equities object by id", () => {
+        getById(60063)
+            .then((resp) => {
+                console.log(resp);
+                // expect(resp.quantity).toEqual(2500);
+            })
+            .catch((ex) => expect(ex).toMatch("error"));
     });
 });
